08660edd11164e540f95c9bd3495b3e5767aba56
Randolph Voorhies <voorhies@usc.edu>
2012-02-25 16:32:15 -0800

    Cleaning up setup

--- setup.py
+++ setup.py
@@ -16,6 +16,20 @@ def installFile(filename, dest):
 
 dotfilesdir = os.getcwd()
 
+# Install oh-my-zsh if it doesnt already exist
+if not os.path.exists(os.path.join(os.environ["HOME"], '.oh-my-zsh')):
+  print 'Installing oh-my-zsh'
+  os.system('curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh')
+
+# Install every file that ends with 'rc' 
 for dotfile in os.listdir(dotfilesdir):
   if dotfile[-2:] == "rc":
     installFile(dotfile, os.environ["HOME"])
+
+# Create a new blank .pathrc if one doesnt exist
+if not os.path.exists(os.path.join(os.environ["HOME"], '.pathrcc'))
+  f = open(os.path.join(os.environ["HOME"], '.pathrcc'), 'w')
+  f.write('# This file is for machine specific paths\n\n\n')
+  f.write('# vim:syntax=sh')
+  f.close()
+
