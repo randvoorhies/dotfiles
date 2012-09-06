29d970c5283229877f0a3b10f2b7050cc8196113
Sagar Pandya <sagargp@gmail.com>
2012-09-06 16:48:22 -0700

    Refactor
    renamed things to *.install
    added git plugin for bash

--- setup.py
+++ setup.py
@@ -3,14 +3,15 @@ import os
 
 def installFile(filename, dest):
   source = os.path.abspath(os.path.join(dotfilesdir, filename))
-  destination = os.path.abspath(os.path.join(dest, '.'+filename))
-  if os.path.exists(destination):
-    if os.path.islink(destination):
-      print 'Removing old symlink: ' + destination
-      os.remove(destination)
-    else:
-      print 'Backing up old file: ' + destination + ' to ' + destination + '.bak'
-      os.rename(destination, destination + '.bak')
+  destination = os.path.abspath(os.path.join(dest, '.'+filename[:-8]))
+
+  if os.path.islink(destination):
+    print 'Removing old symlink: ' + destination
+    os.remove(destination)
+  elif os.path.ispath(destination):
+    print 'Backing up old file: ' + destination + ' to ' + destination + '.bak'
+    os.rename(destination, destination + '.bak')
+
   print 'Installing ' + destination
   os.symlink(source, destination)
 
@@ -18,5 +19,9 @@ dotfilesdir = os.getcwd()
 hostname = os.uname()[1].split('.')[0]
 
 for dotfile in os.listdir(dotfilesdir):
-  if dotfile[-2:] == "rc" or dotfile[:-3] == hostname:
-    installFile(dotfile, os.environ["HOME"])
+  if dotfile[-7:] == "install":
+    installFile(dotfile, os.path.join(os.environ["HOME"]))
+
+for dotfile in os.listdir(dotfilesdir + '/hostnames'):
+  if dotfile[-7:] == "install" and dotfile[:-8] == hostname:
+    installFile(dotfile, os.path.join(os.environ["HOME"]))
