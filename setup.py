81f66aa3b28c25a20dbb5d05292c8604b2708bd8
Sagar Pandya <sagargp@gmail.com>
2012-10-10 12:36:02 -0700

    properly links files in "hostnames" folder

--- setup.py
+++ setup.py
@@ -3,25 +3,30 @@ import os
 
 def installFile(filename, dest):
   source = os.path.abspath(os.path.join(dotfilesdir, filename))
+
+  if '/' in filename:
+    filename = filename.split('/')[-1]
+
   destination = os.path.abspath(os.path.join(dest, '.'+filename[:-8]))
 
   if os.path.islink(destination):
     print 'Removing old symlink: ' + destination
     os.remove(destination)
-  elif os.path.ispath(destination):
+  elif os.path.exists(destination):
     print 'Backing up old file: ' + destination + ' to ' + destination + '.bak'
     os.rename(destination, destination + '.bak')
 
-  print 'Installing ' + destination
+  print 'Installing ' + source + ' at ' + destination
   os.symlink(source, destination)
 
-dotfilesdir = os.getcwd()
-hostname = os.uname()[1].split('.')[0]
+if __name__ == '__main__':
+  dotfilesdir = os.getcwd()
+  hostname = os.uname()[1].split('.')[0]
 
-for dotfile in os.listdir(dotfilesdir):
-  if dotfile[-7:] == "install":
-    installFile(dotfile, os.path.join(os.environ["HOME"]))
+  for dotfile in os.listdir(dotfilesdir):
+    if dotfile[-7:] == "install":
+      installFile(dotfile, os.path.join(os.environ["HOME"]))
 
-for dotfile in os.listdir(dotfilesdir + '/hostnames'):
-  if dotfile[-7:] == "install" and dotfile[:-8] == hostname:
-    installFile(dotfile, os.path.join(os.environ["HOME"]))
+  for dotfile in os.listdir(dotfilesdir + '/hostnames'):
+    if dotfile[-7:] == "install" and dotfile[:-8] == hostname:
+      installFile('hostnames/' + dotfile, os.path.join(os.environ["HOME"]))
